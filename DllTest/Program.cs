ff6ca8558060c2d94e71a23af3a8331d0d986ed1
ddublon <d.dublon@alphaomega-eng.com>
2023-06-29 22:42:59 +0300

    feat: output 4000 arr of numbers

--- DllTest/Program.cs
+++ DllTest/Program.cs
@@ -3,11 +3,19 @@ using System.Runtime.InteropServices;
 
 class Program
 {
-    [DllImport("MyLibrary.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void PrintString(string str);
+    [DllImport("MYLibrary.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void FillArrayWithRandomNumbers(int[] arr, UIntPtr size);
+
 
     static void Main()
     {
-        PrintString("Hello, World! Dolev");
+        int[] arr = new int[4000];
+        FillArrayWithRandomNumbers(arr, (UIntPtr)arr.Length);
+       Console.WriteLine("arr output is :");
+        for(int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
     }
 }
--- MyLibrary.cpp
+++ MyLibrary.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-#include <string>
- 
+#include <random>
+
 extern "C"
 {
-    __declspec(dllexport) void PrintString(const char* str)
+    __declspec(dllexport) void FillArrayWithRandomNumbers(int* arr, size_t size)
     {
-        std::string input(str);
-        std::cout << input << std::endl;
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distr(0, 100);  // generates random numbers between 0 and 100
+
+        for (size_t i = 0; i < size; ++i)
+        {
+            arr[i] = distr(gen);
+        }
+
+        std::cout << "4000 numbers returned" << std::endl;
     }
 }
